ad2e1ff55b8b3d588b5d83590a7ea81719e1aac0
Joy Molomo <jjmolomo17@gmail.com>
2026-01-23 15:39:46 +0200

    app.js and index.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import CartPage from "./pages/CartPage";
+import Home from "./pages/Home";
+import ProductDetail from "./pages/ProductDetail";
+import Checkout from "./pages/Checkout";
+import OrderSuccess from "./pages/OrderSuccess";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello World
-      </h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product/:id" element={<ProductDetail />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/order-success" element={<OrderSuccess />} />
+      </Routes>
+    </Router>
   );
 }
 
