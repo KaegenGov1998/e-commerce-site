6e606c0db7678295bde6cd8718b413351e0682bf
Joy Molomo <jjmolomo17@gmail.com>
2026-02-04 14:29:32 +0200

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 // src/App.jsx
-import React from 'react';
-import CartSummary from './pages/CartSummary';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import CartPage from "./pages/CartPage";
+import CheckoutForm from "./pages/CheckoutForm";
+import OrderSuccess from "./pages/OrderSuccess";
 
-const App = () => {
-  return <CartSummary />;
-};
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/checkout" element={<CheckoutForm />} />
+        <Route path="/order-success" element={<OrderSuccess />} />
+      </Routes>
+    </Router>
+  );
+}
 
 export default App;
